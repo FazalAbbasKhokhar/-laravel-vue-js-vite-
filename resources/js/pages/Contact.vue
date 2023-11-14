98818bd09a743807b64e15d6229602e91fbe738d
Fazal Abbas <fazalabbaskhokhar5@gmail.com>
2023-11-14 11:30:12 +0500

    Pri Quantity Total amount Practice

--- resources/js/pages/Contact.vue
+++ resources/js/pages/Contact.vue
@@ -1,10 +1,39 @@
 <template>
     <div>
-        <h1>Contact Page {{ name }} {{ qty }}
-        <button @click="decrement">-</button>
-        <button @click="increment">+</button>
-        <button @click="changeNameValue('new user value')">click me to change the value of the name</button></h1>
-        <ExampleComponent/>
+        <!-- <h1>Contact Page {{ name }} {{ qty }}
+            <button @click="changeNameValue('new user value')">click me to change the value of the name</button></h1> -->
+
+            <!-- Input  data processing -->
+
+            <!-- For Qty Update -->
+
+            <!-- <input type="number" readonly v-model="qty" @input="checkQtyValue">
+            <button @click="decrement" :disabled="qty == 1">-</button>
+            <button @click="increment">+</button>
+
+        <input type="text" v-model="name" @keypress="showAlert">
+
+        {{ name }} -->
+
+        <!-- <ExampleComponent/> -->
+
+
+        <!-- Process for multiply price with qty and display the total amount in next input -->
+
+        <div style="display:flex;align-items:center">
+            <input type="text" v-model="price" @input="updateTotalAmount">
+            <div style="display:flex;flex-direction:row;align-items:center">
+                <input type="text" v-model="qty" @input="updateTotalAmount">
+                <div style="display:flex;flex-direction:row;align-items:center">
+                    <button @click="decrement" :disabled="qty == 1">-</button>
+                    <button @click="increment">+</button>
+                </div>
+            </div>
+            <input type="text" :readonly="price < 100" v-model="total_amount">
+            <!-- <input type="text" readonly :value="price*qty"> -->
+        </div>
+
+
     </div>
 </template>
 <script>
@@ -17,6 +46,8 @@ export default {
         return {
             name: 'User',
             qty: 1,
+            price: 2000,
+            total_amount: null
         }
     },
 
@@ -36,12 +67,32 @@ export default {
         decrement()
         {
             this.qty--;
+            this.updateTotalAmount();
         },
 
         increment()
         {
             this.qty++;
+            this.updateTotalAmount();
         },
+
+        showAlert()
+        {
+            alert("Input Value changed:" + this.name);
+        },
+
+        checkQtyValue()
+        {
+            if (this.qty <= 0) {
+                this.qty = 1;
+            }
+        },
+
+        updateTotalAmount()
+        {
+            console.log(this.price, this.qty);
+            this.total_amount = this.price * this.qty;
+        }
     },
 }
 </script>
--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Vue & Laravel + Vite</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script> --}}
+
     @vite('resources/css/app.css')
 
 </head>
